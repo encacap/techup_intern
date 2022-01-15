4ecd227db7b1e141be1575bf732783705642832a
Nguyen Khac Khanh <encacap@gmail.com>
2022-01-15 10:36:58 +0700

    Fix getTodoById bugs

--- todoapp_backend/src/services/todo.service.js
+++ todoapp_backend/src/services/todo.service.js
@@ -1,5 +1,7 @@
+const httpStatus = require("http-status");
 const { listService } = require(".");
 const { Todo } = require("../models");
+const ApiError = require("../utils/ApiError");
 
 const createTodo = async (userId, listId, todoBody) => {
     const list = await listService.getListById(listId);
@@ -23,7 +25,7 @@ const getTodos = async (userId, listId, filters) => {
 const getTodoById = async (todoId, throwError = true) => {
     const todo = await Todo.findById(todoId);
     if (!todo && throwError) {
-        throw new Error("Todo not found");
+        throw new ApiError(httpStatus.NOT_FOUND, "Todo not found");
     }
     return todo;
 };
